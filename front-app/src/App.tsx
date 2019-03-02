b83999e1839febe96826e8d0c1b1578c7ba1d637
tebeco <tebeco@gmail.com>
2019-03-02 22:13:39 +0100

    using react hook reducer

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useReducer, Dispatch } from 'react';
 import './App.css';
-import { Tile } from './Tile';
-import { HubConnectionState } from '@aspnet/signalr';
+import { HubConnectionState, HubConnectionBuilder } from '@aspnet/signalr';
 import { DisconnectedApp } from './DisconnectedApp';
+import { ConnectedApp } from './ConnectedApp';
 
-export type AppProps = { hubConnectionState: HubConnectionState }
+type AppAction = ConnectedAction | DisconnectedAction;
+type DisconnectedAction = {
+  type: 'connected'
+}
+type ConnectedAction = {
+  type: 'disconnected'
+}
+
+type AppState = { isConnected: boolean };
+const initialState: AppState = { isConnected: false };
+
+type AppStateReducer = (state: AppState, action: AppAction) => AppState;
+
+const reducer: AppStateReducer = (state, action): AppState => {
+  switch (action.type) {
+    case 'connected':
+      return { isConnected: true };
+    case 'disconnected':
+      return { isConnected: false };
+    default:
+      return state;
+  }
+}
+
+const getConnection = (endpoint: string, dispacth: Dispatch<AppAction>) => {
+  const hubConnection = new HubConnectionBuilder()
+    .withUrl(endpoint)
+    .build();
+
+  hubConnection.onclose((err) => {
+    dispacth({ type: 'disconnected' });
+  });
+
+  hubConnection
+    .start()
+    .then(() => {
+      dispacth({ type: 'connected' });
+    })
+    .catch((err) => {
+      dispacth({ type: 'disconnected' });
+    });
+};
+
+export type AppProps = { clientsEndpoint: string }
 
 export const App = (props: AppProps) => {
-  if (props.hubConnectionState === HubConnectionState.Connected) {
+  const [state, dispatch] = useReducer(reducer, { isConnected: initialState.isConnected });
+
+  if (!state.isConnected) {
+    setTimeout(() => { }, 800)
+    getConnection(props.clientsEndpoint, dispatch);
+  }
+
+  if (state.isConnected) {
     return (
       <div className="App">
-        <Tile name="coin" />
+        <ConnectedApp />
       </div>
     );
   }
@@ -18,9 +68,7 @@ export const App = (props: AppProps) => {
     return (
       <div className="App">
         <DisconnectedApp />
-      </div >
+      </div>
     );
   }
 }
-
-
--- front-app/src/index.tsx
+++ front-app/src/index.tsx
@@ -3,45 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
-import { HubConnectionBuilder, HubConnectionState } from '@aspnet/signalr';
-import { DisconnectedApp } from './DisconnectedApp';
 
 const clientsEndpoint = "https://localhost:5001/clients"
-let hubConnectionState = HubConnectionState.Disconnected;
 
-const getConnection = (endpoint: string) => {
-    const hubConnection = new HubConnectionBuilder()
-        .withUrl(endpoint)
-        .build();
-
-    return hubConnection;
-};
-
-const onClose = (err: Error | undefined) => {
-    console.log('===================== DISCONNECTED =====================');
-
-    hubConnectionState = HubConnectionState.Disconnected;
-}
-
-const hubConnection = getConnection(clientsEndpoint);
-hubConnection.onclose(onClose);
-hubConnection
-    .start()
-    .then(() => {
-        console.log('===================== CONNECTED =====================');
-        hubConnectionState = HubConnectionState.Connected;
-    })
-    .catch((err) => {
-        console.log('===================== NOT CONNECTED =====================');
-        hubConnectionState = HubConnectionState.Disconnected;
-        ;
-    });
-
-ReactDOM.render(<App hubConnectionState={hubConnectionState} />, document.getElementById('root'));
+ReactDOM.render(<App clientsEndpoint={clientsEndpoint}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
