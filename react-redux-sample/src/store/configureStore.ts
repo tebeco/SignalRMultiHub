e28057c1a337c365c22b88886182c701053c0b9c
tebeco <tebeco@gmail.com>
2019-03-17 12:21:10 +0100

    adding an helper to configure the store

--- react-redux-sample/src/store/configureStore.ts
+++ react-redux-sample/src/store/configureStore.ts
@@ -1,1 +1,12 @@
-export const y = {};
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from '../reducers';
+
+export const configureStore = () => {
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+  );
+
+  return store;
+}
