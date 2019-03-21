3bb9e5d968c5500b62e68793e565fc3a815ac9f2
tebeco <tebeco@gmail.com>
2019-03-21 21:17:30 +0100

    bind the store the the App

--- react-redux-sample/src/index.tsx
+++ react-redux-sample/src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { App } from './components/App';
+import { configureStore } from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Create the store
+const store = configureStore()
+
+// Get the Dom component
+const rootElement = document.getElementById('root');
+
+// Render the app
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
